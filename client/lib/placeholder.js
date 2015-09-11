fd59ddd61ce9d392e5026378a5f3205f32bb0a21
stephan281094 <stephan281094@gmail.com>
2015-09-11 19:04:24 +0200

    Improve randomHex, remove semicolons, clean up CSS

--- client/lib/placeholder.js
+++ client/lib/placeholder.js
@@ -1,18 +1,18 @@
 !(function() {
   // helpers
-  function randomHex() {
-    var random = Math.floor(Math.random() * 16777215).toString(16);
-    return random.length === 6 && random != 'ffffff' ? random : randomHex();
+  function randomHex(options) {
+    var random = Math.floor(Math.random() * 16777215).toString(16)
+    return random.length === 6 && !_.contains(options.ignore, random) ? random : randomHex(options)
   }
 
   this.placeholder = {
     color: function(query) {
-      var elements = document.querySelectorAll(query);
+      var elements = document.querySelectorAll(query)
 
       for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        element.style.background = '#' + randomHex();
-      };
+        var element = elements[i]
+        element.style.background = '#' + randomHex({ignore: 'ffffff'})
+      }
     }
-  };
-})();
+  }
+})()
