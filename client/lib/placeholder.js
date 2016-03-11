eabd7dbe8ca6920aa754b30315541816a8a17c1e
stephan281094 <stephan281094@gmail.com>
2016-03-11 21:02:31 +0100

    Add ecmascript

--- client/lib/placeholder.js
+++ client/lib/placeholder.js
@@ -1,16 +1,16 @@
 !(function() {
   // helpers
-  function randomHex(options) {
-    var random = Math.floor(Math.random() * 16777215).toString(16)
+  const randomHex = (options) => {
+    const random = Math.floor(Math.random() * 16777215).toString(16)
     return random.length === 6 && !_.contains(options.ignore, random) ? random : randomHex(options)
   }
 
   this.placeholder = {
-    color: function(query) {
-      var elements = document.querySelectorAll(query)
+    color: (query) => {
+      let elements = document.querySelectorAll(query)
 
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i]
+      for (let i = 0; i < elements.length; i++) {
+        let element = elements[i]
         element.style.background = '#' + randomHex({ignore: 'ffffff'})
       }
     }
